1f6c7d2362958a474af4bf6200ddf789782de0d5
Admin <aydininf@gmail.com>
2023-09-18 22:29:57 +0400

    1512. Number of Good Pairs

--- src/main/java/com/aydinibrahimov/leetcode/GoodPairs.java
+++ src/main/java/com/aydinibrahimov/leetcode/GoodPairs.java
@@ -1,36 +1,29 @@
 package com.aydinibrahimov.leetcode;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class GoodPairs {
     public static void main(String[] args) {
-        int count=0;
         int[] arr = {1, 2, 3, 1, 1, 3};
-        Set<Integer> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-        for (int a : arr) {
-            list.add(a);
-            set.add(a);
-            map.put(String.valueOf(count), a);
-            count++;
-        }
-        List<List<Integer>> duplicatePairs = list.stream()
-                .flatMap(a -> list.stream().map(b -> Arrays.asList(a, b)))
-                .distinct()
-                .collect(Collectors.toList());
 
-        System.out.println(duplicatePairs);
+        System.out.println(numIdenticalPairs(arr));
 
     }
 
     public static int numIdenticalPairs(int[] nums) {
         int n = nums.length;
+        int count = 0;
         for (int i = 0; i < n; i++) {
-
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) count++;
+            }
         }
-        return 0;
+        return count;
 
     }
+
+
 }

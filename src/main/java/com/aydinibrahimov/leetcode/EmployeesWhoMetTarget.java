323c07d07cb7be0a25363767fb12294f48308bb0
Admin <aydininf@gmail.com>
2023-09-19 00:41:22 +0400

    1512. Number of Good Pairs

---
+++ src/main/java/com/aydinibrahimov/leetcode/EmployeesWhoMetTarget.java
@@ -0,0 +1,14 @@
+package com.aydinibrahimov.leetcode;
+
+import java.util.Arrays;
+
+public class EmployeesWhoMetTarget {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static long numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        return Arrays.stream(hours).filter(h -> h >= target).count();
+    }
+}
--- src/main/java/com/aydinibrahimov/leetcode/GoodPairs.java
+++ src/main/java/com/aydinibrahimov/leetcode/GoodPairs.java
@@ -1,12 +1,29 @@
 package com.aydinibrahimov.leetcode;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GoodPairs {
     public static void main(String[] args) {
+        int sum=0;
         int[] arr = {1, 2, 3, 1, 1, 3};
+        int[][] d= {{1,2,3,4,5,6,7},{8,7,4,5,6,1,2},{0,9,8,2,5,4,7}};
+        List<Integer> list=new ArrayList<>();
+        list=Arrays.stream(d).flatMapToInt(Arrays::stream).boxed().collect(Collectors.toList());
+        for(int a:list){
+            sum+=a;
+        }
 
-        System.out.println(numIdenticalPairsA(arr));
+
+    System.out.println(numIdenticalPairsA(arr));
+        System.out.println(numIdenticalPairsC(arr));
+        System.out.println(list);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 5);
+        map.put(1, 5);
+        System.out.println(map);
+
 
     }
 
@@ -22,7 +39,9 @@ public class GoodPairs {
             }
         }
         return count;
-    }    public static int numIdenticalPairsB(int[] nums) {
+    }
+
+    public static int numIdenticalPairsB(int[] nums) {
         int n = nums.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
@@ -33,5 +52,20 @@ public class GoodPairs {
         return count;
     }
 
+    public static int numIdenticalPairsC(int[] nums) {
+        int count = 0, i = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for (int a : nums) {
+
+            if(map.containsKey(a)){
+                count++;
+            }else{
+                map.put(a, i);
+            }
+            i++;
+        }
+
+        return count;
+    }
 }

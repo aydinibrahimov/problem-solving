6e85505119bff2fb99552efb53b76b9491e3866d
Admin <aydininf@gmail.com>
2023-09-20 11:48:19 +0400

    Leetcode->String->1678 Goal Parser

--- src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
+++ src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
@@ -2,33 +2,21 @@ package com.aydinibrahimov.leetcode;
 
 public class GoalParser {
     public static void main(String[] args) {
-        String s="G()()()()(al)";
-        s =s.replace("()","o");
+        String s = "G()()()()(al)";
+        s = s.replace("()", "o");
 
-        s=s.replace("(al)","al");
+        s = s.replace("(al)", "al");
         System.out.println(s);
         System.out.println(interpret("G()()()()(al)"));
 
-        System.out.println("aabcaa".replace("aa","rrr"));
+        System.out.println("aabcaa".replace("aa", "rrr"));
 
 
     }
 
     public static String interpret(String command) {
-        String ans = "";
-
-        int i = 0;
-        char[] arr = command.toCharArray();
-        for (char a : arr) {
-            if (command.contains("()")) {
-               ans= command.replace("()","o");
-
-            }
-            if (command.contains("(al)")) {
-               ans= command.replace("(al)","al");
-            }
-        }
-
+        command = command.replace("()", "o");
+        command = command.replace("(al)", "al");
         return command;
     }
 }

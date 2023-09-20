ba373174fbcf7f072448790cb6bb62dffed0deec
Admin <aydininf@gmail.com>
2023-09-20 11:26:11 +0400

    1678 Goal Parser

--- src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
+++ src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
@@ -2,11 +2,24 @@ package com.aydinibrahimov.leetcode;
 
 public class GoalParser {
     public static void main(String[] args) {
+        System.out.println(interpret("G()()()()(al)"));
+
 
     }
 
     public static String interpret(String command) {
         String ans = "";
+        int i=0;
+        char[] arr=command.toCharArray();
+        for(char a : arr)
+            if(a=='(' && arr[i+1]==')'){
+                ans.concat("o");
+
+            }else if(a=='(' && arr[i+1]=='a'&& arr[i+2]=='l'&&arr[i+3]==')'){
+                ans.concat("al");
+            }else {
+                ans.concat("G");
+            }
 
         return ans;
     }

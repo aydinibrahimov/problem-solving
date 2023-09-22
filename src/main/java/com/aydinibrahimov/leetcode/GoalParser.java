0d4788c3914db6fdb38e3e29dbcb719c87a3d81a
Admin <aydininf@gmail.com>
2023-09-22 20:56:06 +0400

    Leetcode->Array->1672. Richest Customer Wealth

--- src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
+++ src/main/java/com/aydinibrahimov/leetcode/GoalParser.java
@@ -4,9 +4,16 @@ public class GoalParser {
     public static void main(String[] args) {
 
 
-
-
     }
+
+    //LeecCode link -> https://leetcode.com/problems/richest-customer-wealth/
+    //My LeetCode profile: https://leetcode.com/aydinibrahimov
+    public static int maximumWealth(int[][] accounts) {
+
+
+        return 0;
+    }
+
 
     //LeecCode link -> https://leetcode.com/problems/goal-parser-interpretation/description/
     //My LeetCode profile: https://leetcode.com/aydinibrahimov

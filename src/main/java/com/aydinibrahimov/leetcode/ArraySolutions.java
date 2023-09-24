228dec8ff33ca5fb8451236110822c4389687a6d
Admin <aydininf@gmail.com>
2023-09-25 00:10:51 +0400

    1431. Kids With the Greatest Number of Candies

--- src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
+++ src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
@@ -9,14 +9,20 @@ public class ArraySolutions {
     }
 
     public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        Arrays.sort(candies);
+
         List<Boolean> list = new ArrayList<>();
+        int max=0;
+
         for (int c : candies) {
-            if (c + extraCandies < candies[candies.length - 1]) {
-                list.add(false);
-                continue;
+           if(c>max)max=c;
+        }
+        for (int c : candies){
+            if(c+extraCandies>=max){
+                list.add(true);
             }
-            list.add(true);
+            else {
+                list.add(false);
+            }
         }
         return list;
     }

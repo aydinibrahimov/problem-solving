82fc2e06f8555984253d6f07c09e7e7b72c39f3f
Admin <aydininf@gmail.com>
2023-09-30 15:30:37 +0400

    26 Remove Duplicates from Sorted Array

--- src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
+++ src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
@@ -15,13 +15,13 @@ public class ArraySolutions {
     }
 
     public static int removeDuplicates(int[] nums) {
-        int index = 1;
+        int n = 1;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] != nums[i + 1]) {
-                nums[index++] = nums[i + 1];
+                nums[n++] = nums[i + 1];
             }
         }
-        return index;
+        return n;
     }
 
     public static int sumIndicesWithKSetBits(List<Integer> nums, int k) {

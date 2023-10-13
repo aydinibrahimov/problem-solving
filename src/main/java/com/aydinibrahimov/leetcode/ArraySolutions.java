68b52800411e98f745b29d53ef0778b77cfe7f35
Admin <aydininf@gmail.com>
2023-10-13 22:11:29 +0400

    1720. Decode XORed Array

--- src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
+++ src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
@@ -25,10 +25,10 @@ public class ArraySolutions {
     }
 
     public static int[] decode(int[] encoded, int first) {
-        int[] arr=new int[encoded.length+1];
-        arr[0]=first;
-        for(int i=0;i<encoded.length;i++){
-            arr[i+1]=arr[i]^encoded[i];
+        int[] arr = new int[encoded.length + 1];
+        arr[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            arr[i + 1] = arr[i] ^ encoded[i];
         }
         return arr;
 

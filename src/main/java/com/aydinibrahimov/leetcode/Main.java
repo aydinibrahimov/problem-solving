eeed0657cdf39b8e52b811990a87e7ebb38b0d4e
Admin <aydininf@gmail.com>
2023-10-20 16:49:07 +0400

    -

--- src/main/java/com/aydinibrahimov/leetcode/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/Main.java
@@ -8,18 +8,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-
     }
+
 
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        set.addAll(Arrays.stream(nums).);
-        for (int i : nums) {
-            set.add(i);
+        for (int num : nums) {
+            set.add(num);
         }
         if (set.size() != nums.length) return true;
         return false;
 
     }
+
+
 }
-}
+

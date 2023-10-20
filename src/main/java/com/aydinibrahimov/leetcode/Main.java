97ea6f79a9382bf2b1a9568204ef56ffc835279c
Admin <aydininf@gmail.com>
2023-10-20 18:01:20 +0400

    -

--- src/main/java/com/aydinibrahimov/leetcode/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/Main.java
@@ -6,6 +6,13 @@ import java.util.stream.Collector;
 public class Main {
 
     public static void main(String[] args) {
+        Stack<String> items=new Stack<>();
+        items.push("A");
+        items.push("Y");
+        items.push("D");
+        items.push("I");
+        items.push("N");
+        System.out.println(items);
 
 
     }

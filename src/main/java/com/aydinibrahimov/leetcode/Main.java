46c356ac327e67707e967db4e6dec1799f8d6723
Admin <aydininf@gmail.com>
2023-10-20 15:07:45 +0400

    -

--- src/main/java/com/aydinibrahimov/leetcode/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/Main.java
@@ -1,11 +1,33 @@
 package com.aydinibrahimov.leetcode;
 
+import java.util.*;
+import java.util.stream.Collector;
+
 public class Main {
-    static void myMethod(){
+    static void myMethod() {
         System.out.println("My Method");
     }
 
     public static void main(String[] args) {
-        myMethod();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("foo", "bar");
+
+        String value = map.remove("foo");
+
+        map.put("new_foo", value);
+
     }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.stream(nums).);
+        for (int i : nums) {
+            set.add(i);
+        }
+        if (set.size() != nums.length) return true;
+        return false;
+
+    }
+}
 }

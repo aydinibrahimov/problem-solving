a1b21a90985cefd6cc76df7632ef68f0f8c59ee0
aydinibrahimov <aydincrocusoft@gmail.com>
2023-11-21 14:38:47 +0400

    changelog

--- src/main/java/com/aydinibrahimov/leetcode/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/Main.java
@@ -1,18 +1,23 @@
 package com.aydinibrahimov.leetcode;
 
-import org.yaml.snakeyaml.nodes.CollectionNode;
 
-import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class Main {
 
 
+
     public static void main(String[] args) {
-
-
+   Stream.of(1,2,3,4,5,6,7,8)
+           .filter(i->i%2==0)
+           .map(i->{
+               System.out.println(i);
+               return 2*i;
+           })
+           .forEach(System.out::println);
     }
 
 

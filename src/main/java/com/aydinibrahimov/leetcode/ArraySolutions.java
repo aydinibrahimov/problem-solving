005b2827107451681f667b2ff48d25a1318a8e21
aydinibrahimov <aydininf@gmail.com>
2023-12-17 00:21:26 +0400

    17.12.23 00:21

--- src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
+++ src/main/java/com/aydinibrahimov/leetcode/ArraySolutions.java
@@ -13,11 +13,9 @@ import java.util.stream.Stream;
 
 public class ArraySolutions {
     public static void main(String[] args) {
-        StringBuilder sb1 = new StringBuilder("123456");
-        sb1.subSequence(2, 4);
-        sb1.deleteCharAt(3);
-        sb1.reverse();
-        System.out.println(sb1);
+
+        int [] array={1,3,5,6};
+
     }
 
 

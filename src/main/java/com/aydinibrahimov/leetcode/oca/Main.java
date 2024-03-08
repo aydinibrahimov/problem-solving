40dd4b6c448980431f528cc73ca4a37a02d46432
aydinibrahimov <aydininf@gmail.com>
2024-03-08 22:32:02 +0400

    08.03.2024

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -3,4 +3,16 @@ package com.aydinibrahimov.leetcode.oca;
 public class Main {
 
     String a;
+
+    /**
+     * salam doc.. commnets
+     *
+     *
+     * */
+
+    //salam
+    /*
+    *salam xalq
+    *salam * xalqi
+     */
 }

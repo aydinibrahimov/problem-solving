9d325554aa9260f3bec7096c4316cf207833b990
aydinibrahimov <aydininf@gmail.com>
2024-03-21 12:54:19 +0400

    util date and sql date

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -1,14 +1,15 @@
 package com.aydinibrahimov.leetcode.oca;
 
 
-import java.util.Date;
+import java.util.*;
+import java.sql.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
         Date utilDate = new Date();
-        java.sql.Date sqlDate=new java.sql.Date(100L);
+//        java.sql.Date sqlDate=new java.sql.Date(100L);
 
 
     }

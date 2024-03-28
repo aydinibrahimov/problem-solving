6b74dec1f2b7bda3d42226a6c5cbe4979a9a781e
aydinibrahimov <aydininf@gmail.com>
2024-03-28 23:49:47 +0400

    Instance and local variable

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     int aydin=19;//instance variable
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
     Main mainObject=new Main();
         int aydin=20; //local variable
         System.out.println(aydin);//20

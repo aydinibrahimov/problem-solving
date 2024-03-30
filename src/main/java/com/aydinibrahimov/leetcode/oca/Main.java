7966f3e590b3ff6642e39ee54659e4055d5a930d
aydinibrahimov <aydininf@gmail.com>
2024-03-30 19:04:56 +0400

    oca

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -2,13 +2,21 @@ package com.aydinibrahimov.leetcode.oca;
 
 public class Main {
 
+
+    String color = "red";
+    private void printColor(String color) {
+        color = "purple";
+        System.out.println(color);
+    }
     int aydin = 19;//instance variable
     // ////// Walk my dog
     public static void main(String... args) {
+    new Main().printColor("blue");
 
+        int x = 20; //local variable
+        int y=25;
 
-        int aydin = 20; //local variable
-        System.out.println(5+"eret");//20
+        System.out.println(5+x+"eret"+x+y);//20
         System.out.println(Math.floor(5));
         Main mainObject = new Main();
         System.out.println(mainObject.aydin);//19

079c0da49254046a6e2a920649dfd26f48c1340c
aydinibrahimov <aydininf@gmail.com>
2024-03-30 18:45:25 +0400

    oca

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -1,15 +1,12 @@
 package com.aydinibrahimov.leetcode.oca;
 import pocket.complex.*;
-
-import java.util.Map;
-
 public class Main {
 
     int aydin = 19;//instance variable
     // ////// Walk my dog
     public static void main(String... args) {
 
-        Map
+
         int aydin = 20; //local variable
         System.out.println(aydin);//20
         System.out.println(Math.floor(5));

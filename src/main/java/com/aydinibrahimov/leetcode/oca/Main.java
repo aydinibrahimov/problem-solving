a77904c77ed80e952fecb2cb6e0910eea68a926e
aydinibrahimov <aydininf@gmail.com>
2024-04-08 00:09:13 +0400

    oca

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -3,7 +3,12 @@ package com.aydinibrahimov.leetcode.oca;
 public class Main {
 
     public static void main(String... args) {
+int a=5,b=5;
+ boolean c=false;
 
+if(a==b & c==true){
+    System.out.println("fghj");
+}
 
 
     }

16cc9cf6208c8bdcf6186d67d52a05214f215996
aydinibrahimov <aydininf@gmail.com>
2024-04-07 23:10:04 +0400

    oca

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -3,35 +3,8 @@ package com.aydinibrahimov.leetcode.oca;
 public class Main {
 
     public static void main(String... args) {
-        int a = 5;
-        char c = 'A';
-        int b = a + c;
-
-        int i=5;
-        short h=4;
-        int u=i+h;
-
-        short sh = 1;
-        byte by = 1;
-        //because provided int
-        short result = sh + by;
 
 
-        short x = 10;
-        short y = 3;
-
-        //because provided int
-        short v=x/y;
-
-        double d=5;
-        float k=5;
-        float f=5.4;
-        k++;
-        --k;
-        ++k++; //why?
-
-        System.out.println(b);
-        System.out.println((int) c);
 
     }
 

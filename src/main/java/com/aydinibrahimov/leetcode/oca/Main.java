648626208a775462c6c02efb3d521a0c7dfd703a
aydinibrahimov <aydininf@gmail.com>
2024-04-11 19:53:36 +0400

    oca

--- src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
+++ src/main/java/com/aydinibrahimov/leetcode/oca/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
     public static void main(String... args) {
 
-        int x = 6;
-        boolean y = (x >= 6) || (++x <= 7);
+//        int x = 6;
+//        for (int i = 0; i < 5; i++) {
+//            boolean y = (x >= 6) | (++x <= 7);
+//            System.out.println(x);
+//            System.out.println(y);}
+
+
+            boolean y = false;
+            boolean x = (y = true);
         System.out.println(x);
+
 
     }
 
